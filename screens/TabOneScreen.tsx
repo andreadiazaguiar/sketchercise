1432f5d8d627a63692f75e629ae06b48a99c025f
Fin McCallum <finlaymac@outlook.com>
2022-02-17 15:54:50 +0000

    Changed dimensions of map

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -14,8 +14,8 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       <WebView scalesPageToFit={false}
           bounces={false}
           javaScriptEnabled
-          style={{ height: 10, width: 300 }}
-          source={{html: '<iframe src="https://www.google.com/maps/embed?pb=!1m40!1m12!1m3!1d4476.198482050721!2d-4.302338298593547!3d55.878293121967275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m25!3e2!4m3!3m2!1d55.8824859!2d-4.3033231!4m5!1s0x488845c474d34a87%3A0x1c3f74326659ec6e!2sJelly%20Hill%20Cafe%20Bar%2C%20195%20Hyndland%20Rd%2C%20Glasgow%20G12%209HT!3m2!1d55.8757455!2d-4.3053864!4m3!3m2!1d55.8742204!2d-4.2948547!4m5!1s0x488845cc72f33203%3A0x50a165a3f7d3388d!2zw5JyYW4gTcOzciwgQnlyZXMgUm9hZCwgR2xhc2dvdw!3m2!1d55.8775633!2d-4.289759699999999!4m3!3m2!1d55.8824859!2d-4.3033231!5e0!3m2!1sen!2suk!4v1643565741129!5m2!1sen!2suk" width="300" height="550" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}}/>
+          style={{ height: Dimensions.get('window').height/2, width: Dimensions.get('window').width/1.25 }}
+          source={{html: '<iframe src="https://www.google.com/maps/embed?pb=!1m40!1m12!1m3!1d4476.198482050721!2d-4.302338298593547!3d55.878293121967275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m25!3e2!4m3!3m2!1d55.8824859!2d-4.3033231!4m5!1s0x488845c474d34a87%3A0x1c3f74326659ec6e!2sJelly%20Hill%20Cafe%20Bar%2C%20195%20Hyndland%20Rd%2C%20Glasgow%20G12%209HT!3m2!1d55.8757455!2d-4.3053864!4m3!3m2!1d55.8742204!2d-4.2948547!4m5!1s0x488845cc72f33203%3A0x50a165a3f7d3388d!2zw5JyYW4gTcOzciwgQnlyZXMgUm9hZCwgR2xhc2dvdw!3m2!1d55.8775633!2d-4.289759699999999!4m3!3m2!1d55.8824859!2d-4.3033231!5e0!3m2!1sen!2suk!4v1643565741129!5m2!1sen!2suk" width="'+Dimensions.get('window').width+'" height="'+Dimensions.get('window').height/2+'" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}}/>
           
     </View>
   );
